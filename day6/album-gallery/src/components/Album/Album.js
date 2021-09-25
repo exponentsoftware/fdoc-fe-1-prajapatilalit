64e166f16a53fc5ccebb9aac0ab1e1f0b109686a
prajapatilalit <prajapatilalit150992@gmail.com>
2021-09-26 00:23:10 +0530

    update album gallery style

--- day6/album-gallery/src/components/Album/Album.js
+++ day6/album-gallery/src/components/Album/Album.js
@@ -4,10 +4,12 @@ const Album = (props) => {
   return (
     <>
       <div className="album">
-        <img src={props.albumCover} alt="Album-cover" className="album_img" />
-        <div className="album_info">
-          <p>{props.album}</p>
-          <span className="album_category">{props.artist}</span>
+        <div className="inside-box">
+          <img src={props.albumCover} alt="Album-cover" className="album_img" />
+          <div className="album_info">
+            <p>{props.album}</p>
+            <span className="album_category">{props.artist}</span>
+          </div>
         </div>
       </div>
     </>
